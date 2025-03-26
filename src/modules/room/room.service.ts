15aa783202a0db358b29a3b3770ec3dd241dfce3
Zubair Sheikh <zubair@xertk.com>
2025-03-26 20:22:18 +0500

    fix: incremental room numbers

--- src/modules/room/room.service.ts
+++ src/modules/room/room.service.ts
@@ -57,7 +57,7 @@ export const createRoom = async (roomBody: NewCreatedRoom): Promise<IRoomDoc> =>
 
   const createdRooms = await Promise.all(
     Array.from({ length: quantity }, async (_, index) => {
-      const newRoom = { ...room, number: roomCount + quantity - index };
+      const newRoom = { ...room, number: roomCount + index + 1 };
       return Room.create(newRoom).then((r) => r.populate(roomPopulate));
     })
   );
