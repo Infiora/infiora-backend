4cfbbcea4f17a542713359aea0794481a254644b
Zubair <zubair.work3@gmail.com>
2025-10-23 04:36:05 +0500

    fix: file upload issue

--- src/modules/utils/multerUpload.ts
+++ src/modules/utils/multerUpload.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import multer from 'multer';
-import path from 'path';
 
 const storage = multer.diskStorage({
   destination(_req: any, _file: any, cb: any) {
@@ -12,21 +11,11 @@ const storage = multer.diskStorage({
   },
 });
 
-function checkFileType(file: any, cb: any) {
-  const filetypes = /jpg|jpeg|png|gif|bmp|svg|webp/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    return cb(null, true);
-  }
-  return cb(new Error('Only .png, .jpg, .jpeg and pdf format allowed!'));
-}
-
+// ✅ Allow all files
 const multerUpload = multer({
   storage,
-  fileFilter: (_req: any, file: any, cb: any) => {
-    checkFileType(file, cb);
+  fileFilter: (_req: any, _file: any, cb: any) => {
+    cb(null, true); // accept everything
   },
 });
 
